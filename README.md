c305893ebc917215ba1bca385a30193970d85697
Zain Kergaye <zain@zkergaye.me>
2026-09-23 12:10:29 -0600

    init: all

---
+++ README.md
@@ -0,0 +1,4 @@
+# RTOS 00
+
+[![Build firmware](https://github.com/ZainKergayeProjects/rtos.00/actions/workflows/build.yml/badge.svg)](https://github.com/ZainKergayeProjects/rtos.00/actions/workflows/build.yml)
+[![Unit Tests](https://github.com/ZainKergayeProjects/rtos.00/actions/workflows/test.yml/badge.svg)](https://github.com/ZainKergayeProjects/rtos.00/actions/workflows/test.yml)
---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs.rtos-nix.url = "github:ZainKergayeProjects/rtos.nix";
+  inputs.rtos-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs =
+    {
+      self,
+      rtos-nix,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = rtos-nix.nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: rtos-nix.nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (system: {
+        default = pkgs.${system}.stdenv.mkDerivation {
+          name = "lab00";
+          src = ./.;
+          buildInputs = with pkgs.${system}; [
+            cmake
+            git
+            gcc-arm-embedded
+            python3
+            rtos-nix.packages.${system}.pico-sdk-overriden
+            picotool
+            unity-test
+						pioasm
+          ];
+          phases = [ "installPhase" ];
+          installPhase = ''
+						export PICO_SDK_PATH=${rtos-nix.packages.${system}.pico-sdk-overriden}/lib/pico-sdk
+						export FREERTOS_PATH=${rtos-nix.freertos}
+						export OPENOCD_PATH=${pkgs.${system}.openocd}
+						export UNITY_PATH=${rtos-nix.unity}
+						mkdir -p $out
+						cmake -B $out -S $src/ -DCMAKE_BUILD_TYPE=Debug 
+						cd $out
+						cmake --build . --target all -j6
+					'';
+        };
+
+      });
+
+      devShells = forAllSystems (system: {
+        default = rtos-nix.devShells.${system}.default;
+      });
+    };
+}
